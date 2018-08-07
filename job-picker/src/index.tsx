f81a63923aec34882f4d0410f4728a5fddcc8ffb
HENDRICKS <nmhendri@gmail.com>
2018-08-07 07:06:47 -0400

    updated to reflect sp ui req

--- job-picker/src/index.tsx
+++ job-picker/src/index.tsx
@@ -1,13 +1,18 @@
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+
+import 'raf/polyfill';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App, { IAppProps } from './App';
 import './index.scss';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
 const appProps: IAppProps = { appTitle: 'Job Viewer' }
 
 ReactDOM.render(
   <App {...appProps} />,
-  document.getElementById('root') as HTMLElement
+  document.getElementById('spJobPickerRoot') as HTMLElement
 );
-registerServiceWorker();
+//registerServiceWorker();
